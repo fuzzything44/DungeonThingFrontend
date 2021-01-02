b57e62bf87eba779c84908f0d6fd7befa004f216
fuzzything44 <slimesftw@gmail.com>
2021-01-01 22:04:54 -0700

    Include actual built files

---
+++ build/precache-manifest.28a8c16a3c6c81cbd8c193ff435af808.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4187c8a4718109ad05b90167bd00e1f1",
+    "url": "/TavernCellar/index.html"
+  },
+  {
+    "revision": "6dc2f3eae51cf008eaa8",
+    "url": "/TavernCellar/static/css/main.3707af3f.chunk.css"
+  },
+  {
+    "revision": "66545fb0a6901df291ae",
+    "url": "/TavernCellar/static/js/2.35a2185a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TavernCellar/static/js/2.35a2185a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc2f3eae51cf008eaa8",
+    "url": "/TavernCellar/static/js/main.43a946bb.chunk.js"
+  },
+  {
+    "revision": "3f5c61ac9c7e805b3c3e",
+    "url": "/TavernCellar/static/js/runtime-main.0792966a.js"
+  },
+  {
+    "revision": "449c05a59d0a3a58e7ba54f67c1a6263",
+    "url": "/TavernCellar/static/media/1.449c05a5.png"
+  },
+  {
+    "revision": "f945e4be0ea7cb6362c80854b070ea66",
+    "url": "/TavernCellar/static/media/10.f945e4be.png"
+  },
+  {
+    "revision": "fb8874bed8040e136c5d4ecc5bd313ac",
+    "url": "/TavernCellar/static/media/10.fb8874be.png"
+  },
+  {
+    "revision": "95be9f428518aa2ff0dcbfd146a4885f",
+    "url": "/TavernCellar/static/media/11.95be9f42.png"
+  },
+  {
+    "revision": "fb810ceb5fe80b8c1854a9d8f2f66803",
+    "url": "/TavernCellar/static/media/11.fb810ceb.png"
+  },
+  {
+    "revision": "4a2f0a527c45b1aef7f76dc0eb85e2da",
+    "url": "/TavernCellar/static/media/12.4a2f0a52.png"
+  },
+  {
+    "revision": "f732e84051bedba1b6b31fc7d2c1909f",
+    "url": "/TavernCellar/static/media/12.f732e840.png"
+  },
+  {
+    "revision": "6174a6081242cf8c311eeaf9d8f8ba45",
+    "url": "/TavernCellar/static/media/13.6174a608.png"
+  },
+  {
+    "revision": "ece1f7e3bbefecaabd19930146352ce8",
+    "url": "/TavernCellar/static/media/13.ece1f7e3.png"
+  },
+  {
+    "revision": "003fa2c16cc7da7fc9aca412b4abaf1c",
+    "url": "/TavernCellar/static/media/14.003fa2c1.png"
+  },
+  {
+    "revision": "ed4dd6246b1e3e3b1e41226fe43c2c8e",
+    "url": "/TavernCellar/static/media/14.ed4dd624.png"
+  },
+  {
+    "revision": "99c5df68ebacfaad2250b6ad79834f09",
+    "url": "/TavernCellar/static/media/15.99c5df68.png"
+  },
+  {
+    "revision": "b965124dc1b0c4d13274757ac19fcdcb",
+    "url": "/TavernCellar/static/media/15.b965124d.png"
+  },
+  {
+    "revision": "138f683a4ac94d7e286da68f66d218a6",
+    "url": "/TavernCellar/static/media/16.138f683a.png"
+  },
+  {
+    "revision": "8f8436149ed1199a8b154f73cd02500f",
+    "url": "/TavernCellar/static/media/17.8f843614.png"
+  },
+  {
+    "revision": "bc459299023d89a2c2211f283fad5eb0",
+    "url": "/TavernCellar/static/media/18.bc459299.png"
+  },
+  {
+    "revision": "44356581dd5d62ba0440c9e65cc627a4",
+    "url": "/TavernCellar/static/media/19.44356581.png"
+  },
+  {
+    "revision": "84d9d15ae9b2f10467b294e99855e069",
+    "url": "/TavernCellar/static/media/2.84d9d15a.png"
+  },
+  {
+    "revision": "4f765a310eafcf9245c2fb86f09a27f7",
+    "url": "/TavernCellar/static/media/20.4f765a31.png"
+  },
+  {
+    "revision": "ae85e52211299507e3ab826eb623982c",
+    "url": "/TavernCellar/static/media/21.ae85e522.png"
+  },
+  {
+    "revision": "20f67dac0553e2eb7dc81cb83a2f1b73",
+    "url": "/TavernCellar/static/media/22.20f67dac.png"
+  },
+  {
+    "revision": "b58edf20f58913e9fdafe5c3ac1ff0e3",
+    "url": "/TavernCellar/static/media/23.b58edf20.png"
+  },
+  {
+    "revision": "1ba1649964cc9b3a4e41ef51d77ec198",
+    "url": "/TavernCellar/static/media/24.1ba16499.png"
+  },
+  {
+    "revision": "560192ad07f49970411d99a945df9a9f",
+    "url": "/TavernCellar/static/media/3.560192ad.png"
+  },
+  {
+    "revision": "3a2c84d27bc68e4f3fedc9d17ac1515e",
+    "url": "/TavernCellar/static/media/4.3a2c84d2.png"
+  },
+  {
+    "revision": "9c93a897627959b649afee88f8878e89",
+    "url": "/TavernCellar/static/media/5.9c93a897.png"
+  },
+  {
+    "revision": "e5ba36721f2125a634ec430178a32176",
+    "url": "/TavernCellar/static/media/6.e5ba3672.png"
+  },
+  {
+    "revision": "0a4b3b70187fe090fd8d2d3afadfa9b4",
+    "url": "/TavernCellar/static/media/7.0a4b3b70.png"
+  },
+  {
+    "revision": "e0326d7b0a66775b66311f610daba818",
+    "url": "/TavernCellar/static/media/7.e0326d7b.png"
+  },
+  {
+    "revision": "234be19a0ddba20ac1ab2fe0a309185a",
+    "url": "/TavernCellar/static/media/8.234be19a.png"
+  },
+  {
+    "revision": "4df4fc587a51ee6a8beb57838ba34f69",
+    "url": "/TavernCellar/static/media/8.4df4fc58.png"
+  },
+  {
+    "revision": "1dd34acd8fac466a53bfa9af2e8eed6a",
+    "url": "/TavernCellar/static/media/9.1dd34acd.png"
+  },
+  {
+    "revision": "d5dd84e3d995eed8412e710f3bc8c194",
+    "url": "/TavernCellar/static/media/9.d5dd84e3.png"
+  },
+  {
+    "revision": "ea09120c6cd297eabb7b4d79587038ea",
+    "url": "/TavernCellar/static/media/barkeeper.ea09120c.png"
+  },
+  {
+    "revision": "dfa1ee9db946fa561d8a2e622201baab",
+    "url": "/TavernCellar/static/media/mana.dfa1ee9d.png"
+  }
+]);
